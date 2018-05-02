bd0fc16c95d674e8ae6c42875ae3e70eb26c195f
chunxing <chunxing@fit2cloud.com>
2018-05-02 19:10:46 +0800

    Na

--- src/main/java/com/fit2cloud/demo/config/CommonConfig.java
+++ src/main/java/com/fit2cloud/demo/config/CommonConfig.java
@@ -1,13 +1,8 @@
 package com.fit2cloud.demo.config;
 
 import com.zwzx.common.spring.CommonBeanFactory;
-import com.zwzx.common.spring.PropertiesConfigurer;
-import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.core.io.support.ResourcePatternResolver;
 
 /**
  *
