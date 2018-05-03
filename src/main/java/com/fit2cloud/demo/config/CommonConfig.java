dfeb7c9f8c890c334c533d6878e44d3e702929a6
liqiang-fit2cloud <liqiang@fit2cloud.com>
2018-05-03 22:12:27 +0800

    增加web-public依赖

--- src/main/java/com/fit2cloud/demo/config/CommonConfig.java
+++ src/main/java/com/fit2cloud/demo/config/CommonConfig.java
@@ -1,11 +1,13 @@
 package com.fit2cloud.demo.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
  *
  */
 @Configuration
+@ComponentScan(basePackages = {"com.fit2cloud"})
 public class CommonConfig {
 
 }
