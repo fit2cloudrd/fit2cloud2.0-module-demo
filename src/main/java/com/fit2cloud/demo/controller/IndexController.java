c0dc57f41b0c01a153c6b7119aca93c48cfb9d91
q4speed <kun@fit2cloud.com>
2018-06-08 15:31:25 +0800

    修改根路径判断

--- src/main/java/com/fit2cloud/demo/controller/IndexController.java
+++ src/main/java/com/fit2cloud/demo/controller/IndexController.java
@@ -1,9 +1,13 @@
 package com.fit2cloud.demo.controller;
 
+import com.fit2cloud.commons.server.constants.IndexConstants;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
@@ -14,8 +18,13 @@ public class IndexController {
      * 跳转到无Header主页
      */
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(Model model) {
+    public String index(Model model, HttpServletRequest request) {
         model.addAttribute("timestamp", timestamp);
-        return "index";
+        String banner = request.getParameter(IndexConstants.BANNER);
+        if (StringUtils.equals(banner, IndexConstants.BANNER_FALSE)) {
+            return "index";
+        } else {
+            return "redirect:/web-public";
+        }
     }
 }
