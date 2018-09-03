60e76e89c4cde5362fe2c28e0e1565488a587072
dongbin <dongbin@fit2cloud.com>
2018-09-03 16:46:28 +0800

    数据库字段加密

---
+++ src/main/java/com/fit2cloud/demo/config/DemoDBEncryptConfig.java
@@ -0,0 +1,18 @@
+package com.fit2cloud.demo.config;
+
+import com.fit2cloud.commons.server.config.DBEncryptConfig;
+import com.fit2cloud.commons.utils.EncryptConfig;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * 配置加密
+ */
+@Component
+public class DemoDBEncryptConfig implements DBEncryptConfig {
+    @Override
+    public List<EncryptConfig> encryptConfig() {
+        return null;
+    }
+}
