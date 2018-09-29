e94ddb2a878b28bfc95c272f638dfb98a2e68c30
dongbin <dongbin@fit2cloud.com>
2018-09-29 15:36:04 +0800

    修改 日志配置

--- src/main/java/com/fit2cloud/demo/Application.java
+++ src/main/java/com/fit2cloud/demo/Application.java
@@ -4,14 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication(exclude = QuartzAutoConfiguration.class)
 @EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
-        run.getBean("indexController");
+        SpringApplication.run(Application.class, args);
     }
 }
