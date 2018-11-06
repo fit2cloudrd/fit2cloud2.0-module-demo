b30680503ff4df1c1d74509380acbfe3459cc414
dongbin <dongbin@fit2cloud.com>
2018-11-06 17:39:59 +0800

    [Fix] 添加noResultHolder 注解

--- src/main/java/com/fit2cloud/demo/controller/DemoController.java
+++ src/main/java/com/fit2cloud/demo/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.fit2cloud.demo.controller;
 
+import com.fit2cloud.commons.server.handle.annotation.NoResultHolder;
 import com.fit2cloud.commons.utils.ResultHolder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     @RequestMapping(value = "/test1/{module}")
-    public Object noResultHolder(@PathVariable String module) throws InterruptedException {
+    public ResultHolder resultHolder1(@PathVariable String module) throws InterruptedException {
         Thread.sleep(Integer.valueOf(module));
         return ResultHolder.success(module);
     }
 
     @RequestMapping(value = "/test2/{module}")
-    public Object resultHolder(@PathVariable String module) {
+    public String resultHolder2(@PathVariable String module) {
+        return module;
+    }
+
+
+    @RequestMapping(value = "/test3/{module}")
+    @NoResultHolder
+    public String resultHolder3(@PathVariable String module) {
         return module;
     }
 }
